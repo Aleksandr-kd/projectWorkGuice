ca2487c03dcb5da3104ea94fa2aba14f49e63de7
ASArutyunyan <aleks_pochta_krd@mail.ru>
2025-06-23 11:52:53 +0300

    Запуск кода 22.06

--- src/main/java/ru/arutyunyan/factory/WebDriverFactory.java
+++ src/main/java/ru/arutyunyan/factory/WebDriverFactory.java
@@ -5,45 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import ru.arutyunyan.exceptions.BrowserNotSupportedException;
 import ru.arutyunyan.factory.settings.ChromeSettings;
 import ru.arutyunyan.factory.settings.FirefoxSettings;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Map;
-
-import org.openqa.selenium.MutableCapabilities;
 
 
 public class WebDriverFactory {
 
     private final String browserName = System.getProperty("browser");
-    private final String browserVersion = System.getProperty("browserVersion");
-    private String remoteUrl = System.getProperty("remote.url", "");
 
-    public WebDriver createDriver() throws MalformedURLException {
-        if (!remoteUrl.isEmpty()) {
-            MutableCapabilities capabilities = new MutableCapabilities();
-            capabilities.setCapability("browserName", browserName);
-            capabilities.setCapability("browserVersion", browserVersion);
-
-            MutableCapabilities options;
-            switch (browserName.toLowerCase()) {
-                case "chrome" -> options = (ChromeOptions) new ChromeSettings().settings();
-                case "firefox" -> options = (FirefoxOptions) new FirefoxSettings().settings();
-                default -> throw new BrowserNotSupportedException(browserName);
-            }
-
-            capabilities.setCapability("selenoid:options", Map.of(
-                    "enableVNC", true,
-                    "enableVideo", false
-            ));
-            capabilities.merge(options);
-
-            return new RemoteWebDriver(new URL(remoteUrl), capabilities);
-        }
+    public WebDriver createDriver() {
         return switch (browserName.toLowerCase()) {
             case "chrome" -> new ChromeDriver((ChromeOptions) new ChromeSettings().settings());
             case "firefox" -> new FirefoxDriver((FirefoxOptions) new FirefoxSettings().settings());
