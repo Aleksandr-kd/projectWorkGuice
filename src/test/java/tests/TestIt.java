aa20b1bd097df15f2e79f256b7af204c86b1c191
ASArutyunyan <aleks_pochta_krd@mail.ru>
2025-06-24 01:46:01 +0300

    Запуск кода 1

---
+++ src/test/java/tests/TestIt.java
@@ -0,0 +1,52 @@
+package tests;
+
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import ru.arutyunyan.dto.User;
+import ru.arutyunyan.dto.WishList;
+import ru.arutyunyan.extension.TestSetupExtension;
+import ru.arutyunyan.pages.otus.ClientOtusPage;
+import ru.arutyunyan.pages.otus.UsersPage;
+
+
+@ExtendWith(TestSetupExtension.class)
+public class TestIt {
+
+    @Inject
+    private ClientOtusPage clientOtusPage;
+
+    @Inject
+    private UsersPage usersPage;
+
+    @Inject
+    private User user;
+
+    @Inject
+    private WishList wishList;
+
+
+    @Test
+    @Tag("test")
+    @DisplayName("Управление пользовательского подарка. Поиск и удаление подарка.")
+    public void userPresentView() {
+
+        usersPage
+                .open();
+
+        clientOtusPage
+                .registration(user)
+                .authorization(user);
+
+        usersPage
+                .deleteAllWishLists()
+                .buttonDeleteWishListShouldNotBeDisplayed()
+                .clickCreateNewWishList()
+                .formCreateNewWishList(wishList)
+                .clickButtonCreate()
+                .viewWishList()
+                .nameWishListShouldNotBeDisplayed(wishList);
+    }
+}
