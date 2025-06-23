9c7173d58a6c69a41a057b98a9614a398e8bc419
ASArutyunyan <aleks_pochta_krd@mail.ru>
2025-06-24 01:38:32 +0300

    Запуск кода 1

--- src/main/java/ru/arutyunyan/factory/settings/ChromeSettings.java
+++ src/main/java/ru/arutyunyan/factory/settings/ChromeSettings.java
@@ -1,5 +1,6 @@
 package ru.arutyunyan.factory.settings;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.AbstractDriverOptions;
 import ru.arutyunyan.data.BrowserModeData;
@@ -11,6 +12,7 @@ public class ChromeSettings implements IBrowserSettings {
 
     @Override
     public AbstractDriverOptions<?> settings() {
+        WebDriverManager.chromedriver().browserVersion("139.0.7258.0").setup();
         ChromeOptions options = new ChromeOptions();
         BrowserModeData modeData = BrowserModeData.valueOf(mode);
 
